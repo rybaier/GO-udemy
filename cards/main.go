b57e900ecf725d776f8a1f26f68a7cb831363076
rybaier <rybaier@gmail.com>
2022-12-05 10:43:20 -0500

    notes

--- cards/main.go
+++ cards/main.go
@@ -1,5 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
+	// var card string = "Ace of Spades"
+	// define variable in multiple ways with identifying type or
+	// using :=
+	// can initialize variable without assigning a value
+	// can initialize variable outside of function without assigning value
+	// can only assign value to variable within function
+	card := "Ace of Spades"
+	card = "Five of Diamonds"
+
+	fmt.Println(card)
 
 }
